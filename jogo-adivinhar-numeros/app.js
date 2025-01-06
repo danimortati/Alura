d3a424c6d67f14a149f576231928212fbb1aeef5
Daniele Mortati <mortatidani@gmail.com>
2025-01-06 13:24:06 -0300

    aluratest

---
+++ jogo-adivinhar-numeros/app.js
@@ -0,0 +1,43 @@
+let numeroSecreto = gerarNumeroAleatorio ();
+let tentativas = 1;
+
+function exibirTextoNaTela (tag, texto){
+    let campo = document.querySelector (tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela ("h1", "Jogo do número secreto");
+exibirTextoNaTela ("p", "Escolha entre o número 1 e 10");
+
+function verificarChute(){
+
+    console.log (numeroSecreto);
+}
+ function gerarNumeroAleatorio (){
+     return parseInt(Math.random() * 10 + 1);
+ }
+
+ function verificarChute() {
+    let chute = document.querySelector("input").value;
+
+    if (chute == numeroSecreto){
+        exibirTextoNaTela ("h1", "Acertou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela ("p", mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela ("p", "O número secreto é menor");
+        } else {
+            exibirTextoNaTela ("p", "O número secreto é maior");
+        }
+        tentativas++;
+        limparCampo ();
+    }
+ }
+
+ function limparCampo(){
+    chute = document.querySelector ("input");
+    chute.value = '';
+ }
+ 
